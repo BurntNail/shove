95d7dea6142f751d7f09e3aed19c0ba91a091a04
Jack Maguire <jack@maguire.tech>
2024-11-18 19:57:37 +0700

    fix reloading + not found page

--- serve/state.rs
+++ serve/state.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use bloggthingie::{
     aws::{get_bucket, get_upload_data},
     UploadData,
@@ -42,6 +43,14 @@ impl State {
         info!("Got bucket & upload data");
 
         let cache = CacheBuilder::new(1024).support_invalidation_closures().build();
+
+        match Self::read_file_from_s3(format!("{}/404.html", &upload_data.root), &bucket).await {
+            Ok((contents, content_type, path)) => {
+                info!("Adding 404 path to cache");
+                cache.insert(path, (contents, content_type)).await;
+            }
+            Err(e) => warn!(?e, "Error getting 404 page from S3")
+        }
 
         let task_cache = cache.clone();
         let task_bucket = bucket.clone();
@@ -92,40 +101,49 @@ impl State {
 
         *self.upload_data.write().await = new_upload_data.clone();
 
-        self.cache.invalidate_all();
-        //TODO: actual work :)
+
+        let mut to_be_updated: HashSet<String> = new_upload_data.entries.keys().cloned().collect();
+        let mut to_be_removed: Vec<String> = vec![];
+
+        for (old_entry, old_hash) in old_upload_data.entries {
+            match new_upload_data.entries.get(&old_entry) {
+                Some(new_hash) => if &old_hash == new_hash {
+                    to_be_updated.remove(&old_entry);
+                }
+                None => to_be_removed.push(old_entry),
+            }
+        }
+
+        if let Err(e) = self.cache.invalidate_entries_if(move |entry, _| to_be_removed.contains(entry)) {
+            warn!(?e, "Error invalidating old entries")
+        }
+
+        let task_cache = self.cache.clone();
+        let task_bucket = self.bucket.clone();
+        tokio::task::spawn(async move {
+            let mut read_files: FuturesUnordered<_> = to_be_updated
+                .into_iter()
+                .map(|pb| Self::read_file_from_s3(pb.clone(), &task_bucket))
+                .collect();
+
+            while let Some(res) = read_files.next().await {
+                match res {
+                    Ok((contents, content_type, path)) => {
+                        info!(?path, "file changed, updating");
+                        task_cache.insert(path, (contents, content_type)).await;
+                    },
+                    Err(e) => {
+                        warn!(?e, "Error updating file from S3")
+                    }
+                }
+            }
+
+            info!("Updated cache from S3");
+        });
+
+
+
         Ok(())
-
-
-        //
-        // let cloned_entries = new_upload_data.entries.clone();
-        // self.cache.invalidate_entries_if(move |key, _value| {
-        //     !cloned_entries.contains_key(key)
-        // })?;
-        //
-        // let mut read_files: FuturesUnordered<_> = new_upload_data
-        //     .entries
-        //     .keys()
-        //     .map(|pb| Self::read_file_from_s3(pb.to_string(), &self.bucket))
-        //     .collect();
-        //
-        // let mut not_found = None;
-        //
-        // while let Some(res) = read_files.next().await {
-        //     let (contents, content_type, path) = res?;
-        //
-        //     if path.contains("404.html") {
-        //         not_found = Some((contents.clone(), content_type.clone()));
-        //     }
-        //
-        //     self.cache.insert(path, (contents, content_type)).await;
-        // }
-        //
-        // *self.not_found.write().await = not_found;
-        //
-        // info!("Finished reloading cache");
-        //
-        // Ok(())
     }
 
     pub async fn get(&self, path: &str) -> Option<(Vec<u8>, String, StatusCode)> {
@@ -133,7 +151,7 @@ impl State {
         let path = format!("{root}{path}");
 
         let not_found = || async {
-            let (content, content_type) = self.cache.get(&format!("{root:?}404.html")).await?;
+            let (content, content_type) = self.cache.get(&format!("{root}/404.html")).await?;
             Some((content, content_type, StatusCode::NOT_FOUND))
         };
 
